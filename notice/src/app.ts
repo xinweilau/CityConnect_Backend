c7bcb65aab9e0d0cd72c3d8789e6107de9ea8fd2
Lau Xin Wei <xinweii1996@live.com>
2023-08-10 17:59:55 +0800

    feat: add docker-compose setup for notice microservice

--- notice/src/app.ts
+++ notice/src/app.ts
@@ -8,7 +8,7 @@ import noticeRoute from "./routes";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
