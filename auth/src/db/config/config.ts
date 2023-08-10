09735405add5006a7025ffbf35ae300441eb64de
Lau Xin Wei <xinweii1996@live.com>
2023-08-10 17:51:47 +0800

    feat: add docker-compose setup for auth microservice

--- auth/src/db/config/config.ts
+++ auth/src/db/config/config.ts
@@ -1,8 +1,5 @@
 import { Sequelize } from "sequelize";
 
-const DB_URI = process.env.DB_URI || "<CONNECTION STRING>";
-const LOG = true;
-
-export const connection = new Sequelize(DB_URI, {
-  logging: LOG,
+export const connection = new Sequelize(process.env.DATABASE_URL, {
+  logging: true,
 });
